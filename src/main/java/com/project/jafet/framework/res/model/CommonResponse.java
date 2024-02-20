ae2b35dc49db245180226ec596787b67ef197c44
baphomet <rayyanmirza419@gmail.com>
2024-02-21 00:36:27 +0530

    Create CommonResponse.java
    Basic app response for normal http get/post

---
+++ src/main/java/com/project/jafet/framework/res/model/CommonResponse.java
@@ -0,0 +1,13 @@
+package com.project.jafet.framework.res.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommonResponse {
+
+	public String responseMessage;
+	public String responseCode;
+	
+}
