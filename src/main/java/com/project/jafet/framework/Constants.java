a30de213517235451f41a047abba960eb3115d23
baphomet <rayyanmirza419@gmail.com>
2024-02-21 00:34:30 +0530

    Create Constants.java
    contains app-constants

---
+++ src/main/java/com/project/jafet/framework/Constants.java
@@ -0,0 +1,12 @@
+package com.project.jafet.framework;
+
+public class Constants {
+
+	public static final String TRAINED_MODEL="/lbphModel.xml";
+	
+	public static final String HAAR_CLASSIFIER = "/etc/haarcascades/haarcascade_frontalface_default.xml";
+	
+	public static final String TRAINING_DATA="/FaceRecData/Local/";
+	
+	public static final String FACE_SEQ = "FACE_LABEL";
+}
